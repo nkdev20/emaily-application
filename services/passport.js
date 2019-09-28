3cd349e05d0391b60a32b1121a1c69999e0871e5
nikhil <nikhil@sjinnovation.com>
2019-09-28 17:05:35 +0530

    implemented oauth flow

---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose =  require('mongoose');
+const keys = require('../config/keys');
+const User = mongoose.model('users');
+
+//cinfiguring passport
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
+passport.use(
+    new googleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+           User.findOne({googleId:profile.id})
+                .then((existingUser) => {
+                    if(existingUser){   
+                        done(null, existingUser);
+                    } else {
+                        new User({ googleId: profile.id})
+                            .save()
+                            .then(user => done('null', user));
+                    }
+                });
+         
+        }
+    )
+);
