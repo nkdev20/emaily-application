f9d6a2305b756bc19aba64883aef8732cbf0d940
nikhil <nikhil@sjinnovation.com>
2019-09-28 17:22:41 +0530

    minor change

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,7 +18,6 @@ module.exports = (app) => {
     })
 
     app.get('/api/current_user', (req, res) => {
-        res.send(req.session);
-        // res.send(req.user);
+        res.send(req.user);
     });
 }
