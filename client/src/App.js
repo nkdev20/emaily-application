6878b2e2b6d00be5e15060e0c24700304c52c125
nikhil <nikhil@sjinnovation.com>
2019-10-18 09:02:11 +0530

    implemented googel passport startegy and create a react app

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>testing</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a href = "/auth/google">Google Sign in </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
