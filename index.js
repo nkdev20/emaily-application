0dda52c4d3955c9572587a85e9ff671b02584500
nikhil <nikhil@sjinnovation.com>
2019-12-07 12:00:28 +0530

    implemented middleware,display credits and moidified model

--- index.js
+++ index.js
@@ -27,5 +27,20 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 
+
+if(process.env.NODE_ENV === 'production'){
+    //Express will serve production assets like main.jsor main.css file!
+    app.use(express.static('client/build'));
+
+
+
+    //Express will serve index.html file if it does not recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });     
+
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,23 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
+
+        /**
+         * Can access req.user when paspporrt login feature provide by passport
+         */
+
         const charge = await stripe.charges.create({
             amount:500,
             currency:'usd',
             description:'$5 for 5 credits',
             source: req.body.id
         });
-        console.log(charge);
+        
+        req.user.credits += 5;
+        const user = await req.user.save();
+        res.send(user);
     })
 }
